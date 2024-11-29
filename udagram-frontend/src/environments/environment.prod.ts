1b75c6c1c5a14ac28c1371c38815c8f57c43f5a1
letienlocvn <letienlocvn@gmail.com>
2024-11-29 20:07:31 +0700

    Add http

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a7bd46ed5767f4255a5eb30faff99577-1586170067.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a7bd46ed5767f4255a5eb30faff99577-1586170067.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
